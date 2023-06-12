1fba141c1d296e18e970025df41d60c87a4074cd
1willcobb <cobb.will@gmail.com>
2023-06-11 19:58:08 -0700

    added todo categories

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,38 @@ var activatedButton = document.getElementsByClassName("platforms");
 activatedButton.addEventListener("click", (){
 
 })
+
+
+//*** Social Widgets ****/
+//! YouTube
+//! Meta (FB and IG)
+//! TikTok
+//! Twitch
+//! Snap Chat
+//! Patreon
+
+
+//*** Online Courses Widgets ****/
+//! Teachable
+//! Kajabi
+//! Skillshare
+//! Udemy
+//! Thinkific
+
+//*** Music Widgets ****/
+//! Spotify
+//! Apple Music
+
+//*** E-Commerce Widgets ****/
+//! Shopify
+//! Wix
+//! Squarespace
+//! Amazon
+
+//*** Merch Widgets ****/
+//! T-Spring
+//! Threads
+//! UberPrints
+//! Vista Print
+//! Awesome Merch
+//! Captiv8
