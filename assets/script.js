2f5434d10ef917756950930ca86c52125ccdd34a
1willcobb <cobb.will@gmail.com>
2023-06-14 10:19:04 -0700

    turned all api calls into functions and created button listners for the 3 working apis. created a placeolder data structure for new strucutres and created a filled out data structure to act as an example.

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,17 @@ const endDate = dayjs().endOf('month').format('YYYY-MM-DD');
 const currentMonth = dayjs().month();
 const options_btn = document.getElementById('options_btn');
 const submit = document.getElementById('submit');
-const options_container = document.getElementById('options_container')
-const user_input_modal = document.getElementById('user_input')
-const modal_content = document.getElementById('modal-content')
+const options_container = document.getElementById('options_container');
+const user_input_modal = document.getElementById('user_input');
+const modal_content = document.getElementById('modal-content');
 
 //* GLOBALS
-var youtube_channel_name;
+
 
 //* Options Buttons
-const youtube_btn = document.getElementById('youtube')
+const youtube_btn = document.getElementById('youtube');
+const teachable_btn = document.getElementById('teachable');
+const reverb_btn = document.getElementById('reverb');
 
 //* Data Structure for local storage
 var dataStructure = {
@@ -32,6 +34,23 @@ var dataStructure = {
   }
 }
 
+var placeholderDataStructure = {
+  youtube: {
+    widget_name: 'youtubeWidget',
+    apiKey: '',
+    channelName: '',
+    channelId: '',
+  },
+  teachable: {
+    widget_name: 'teachableWidget',
+    apiKey: ''
+  },
+  reverb:{
+    widget_name: 'reverbWidget',
+    apiKey: ''
+  }
+}
+
 //! Local Storage
 
 api_storage = getStorage();
@@ -44,7 +63,7 @@ function getStorage(){
   if (api_storage) {
     api_storage = JSON.parse(api_storage);
   } else {
-    api_storage = dataStructure;
+    api_storage = placeholderDataStructure;
     options_container.removeAttribute("class", "hide")
   }
   return api_storage;
@@ -55,47 +74,113 @@ function setStorage(api_storage) {
 }
 
 //* Options Listeners
+
+//? Youtube Options
 youtube_btn.addEventListener("click", function(){
   user_input_modal.classList.add('is-active');
   modal_content.innerHTML = '<form id="youtube_form">' +
   '<label>Channel Name</label>' +
-  '<input id="channel_input" placeholder="Mr Beast">' +
-  '</input><button type="submit">Submit</button>' +
+  '<input id="channel_input" placeholder="Mr.Beast"></input>' +
+  '<button type="submit">Submit</button>' +
   '</form>';
 
-  // Add form submission listener
+  // Add form submission listener to created elements
   var youtubeForm = document.getElementById('youtube_form');
   youtubeForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission from refreshing the page
 
-    // Get the input value
+    // Get the input value from created elements
     var channelInput = document.getElementById('channel_input');
     api_storage.youtube.channelName = channelInput.value;
 
-    // Do something with the channelName value
-    console.log('Channel Name:', api_storage.youtube.channelName);
-    createYoutubeWidget();
+    // create the widget and set the storage varaibles
     setStorage(api_storage);
+    api_storage = getStorage();
+    createYoutubeWidget();
 
-
-    // Close the modal if needed
+    // Close the modal
     user_input_modal.classList.remove('is-active');
+    hideOptions();
   });
-  whatisName();
+})
+
+//? Teachable Options
+teachable_btn.addEventListener("click", function(){
+  user_input_modal.classList.add('is-active');
+  modal_content.innerHTML = '<form id="teachable_form">' +
+  '<label>Unique API key</label>' +
+  '<input id="teachable_api_key" placeholder="74h29183hh48390204845ubBHHDhak"></input>' +
+  '<button type="submit">Submit</button>' +
+  '</form>';
+
+  // Add form submission listener to created elements
+  var teachable_form = document.getElementById('teachable_form');
+  teachable_form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission from refreshing the page
+
+    // Get the input value from created elements
+    var teachable_api_key = document.getElementById('teachable_api_key');
+    api_storage.teachable.apiKey = teachable_api_key.value;
+
+    // create the widget and set the storage varaibles
+    setStorage(api_storage);
+    api_storage = getStorage();
+    createTeachableWidget();
+
+    // Close the modal
+    user_input_modal.classList.remove('is-active');
+    hideOptions();
+  });
+})
+
+//? Reverb Options
+reverb_btn.addEventListener("click", function(){
+  user_input_modal.classList.add('is-active');
+  modal_content.innerHTML = '<form id="reverb_form">' +
+  '<label>Unique API key</label>' +
+  '<input id="reverb_api_key" placeholder="74h29183hh48390204845ubBHHDhak"></input>' +
+  '<button type="submit">Submit</button>' +
+  '</form>';
+
+  // Add form submission listener to created elements
+  var reverb_form = document.getElementById('reverb_form');
+  reverb_form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission from refreshing the page
+
+    // Get the input value from created elements
+    var reverb_api_key = document.getElementById('reverb_api_key');
+    api_storage.reverb.apiKey = reverb_api_key.value;
+
+    // create the widget and set the storage varaibles
+    setStorage(api_storage);
+    api_storage = getStorage();
+    createTeachableWidget();
+
+    // Close the modal
+    user_input_modal.classList.remove('is-active');
+    hideOptions();
+  });
 })
 
 
 
 
+function showOptions(){
+  options_container.removeAttribute("class", "hide");
+};
+
+function hideOptions(){
+  options_container.classList.add('hide')
+};
 
 submit.addEventListener("click", function(){
-  options_container.classList.add("hide")
-})
+  hideOptions();
+});
 
 
 options_btn.addEventListener("click", function(){
-  options_container.removeAttribute("class", "hide")
-})
+  showOptions();
+});
 
 
 
@@ -137,6 +222,7 @@ function createYoutubeWidget() {
             var videoCount = data.items[0].statistics.videoCount
             var viewCount = data.items[0].statistics.viewCount
 
+            //? Generate Youtube Widget
             const ytWidget = document.createElement("div");
             ytWidget.setAttribute("class", "widget");
             ytWidget.innerHTML = '<h1>YouTube</h1>' + 
@@ -151,9 +237,6 @@ function createYoutubeWidget() {
 }
 
 
-
-
-
 //! Meta (FB and IG)
 //! TikTok 
 //! Twitch
@@ -164,54 +247,58 @@ function createYoutubeWidget() {
 //*** Online Courses Widgets ****/
 
 //! Teachable ///////////////
-const options = {
-  method: 'GET',
-  headers: {accept: 'application/json', apiKey: 'B0RD3UreiCuqORd5bau5CKx3MC0s4pTN'}
-};
-
-var total_rev;
-
-//? get total rev
-fetch('https://developers.teachable.com/v1/transactions?per=10000', options)
-  .then(function (response){
-    return response.json();
+function createTeachableWidget() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json', 
+      apiKey: `${api_storage.teachable.apiKey}`}
+  };
+  
+  var total_rev;
+  
+  //? get total rev
+  fetch('https://developers.teachable.com/v1/transactions?per=10000', options)
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (data) {
+      total_rev = 0;
+      for (var i in data['transactions']){
+          total_rev += data['transactions'][i].revenue
+      }
+      
+      //Move the decimal 
+      total_rev = total_rev / 100
+      console.log("(Teachable) Total Revenue TD: "+ total_rev)
+  
+      //? get monthly rev
+      fetch('https://developers.teachable.com/v1/transactions?start=' + startDate + '&end=' + endDate, options)
+        .then(function (response){
+          return response.json();
+        })
+        .then(function (data){
+          var monthly_rev = 0;
+          for (var i in data['transactions']){
+              monthly_rev += data['transactions'][i].revenue
+          }
+          
+          //Move the decimal 
+          monthly_rev = monthly_rev / 100
+          console.log("(Teachable) Monthly Revenue: " + monthly_rev)
+  
+          //? Generate Teachable Widget
+          const teachWidget = document.createElement("div");
+          teachWidget.setAttribute("class", "widget");
+          teachWidget.innerHTML = '<h1>Teachable</h1>' + 
+          '<div>Monthly Revenue: $'+ monthly_rev + '</div>' +
+          '<div>Total Revenue TD: $'+ total_rev + '</div>' + 
+          '<br/>'
+  
+          test_widget.appendChild(teachWidget)
+    })
   })
-  .then(function (data) {
-    total_rev = 0;
-    for (var i in data['transactions']){
-        total_rev += data['transactions'][i].revenue
-    }
-    
-    //Move the decimal 
-    total_rev = total_rev / 100
-    console.log("(Teachable) Total Revenue TD: "+ total_rev)
-
-    //? get monthly rev
-    fetch('https://developers.teachable.com/v1/transactions?start=' + startDate + '&end=' + endDate, options)
-      .then(function (response){
-        return response.json();
-      })
-      .then(function (data){
-        var monthly_rev = 0;
-        for (var i in data['transactions']){
-            monthly_rev += data['transactions'][i].revenue
-        }
-        
-        //Move the decimal 
-        monthly_rev = monthly_rev / 100
-        console.log("(Teachable) Monthly Revenue: " + monthly_rev)
-
-        //? Generate Teachable Widget
-        const teachWidget = document.createElement("div");
-        teachWidget.setAttribute("class", "widget");
-        teachWidget.innerHTML = '<h1>Teachable</h1>' + 
-        '<div>Monthly Revenue: $'+ monthly_rev + '</div>' +
-        '<div>Total Revenue TD: $'+ total_rev + '</div>' + 
-        '<br/>'
-
-        test_widget.appendChild(teachWidget)
-  })
-})
+}
 
 
 //! Kajabi
@@ -229,38 +316,40 @@ fetch('https://developers.teachable.com/v1/transactions?per=10000', options)
 //! Squarespace
 //! Amazon
 //! REVERB
-const reverb_api = 'https://api.reverb.com/api/my/payments/selling'
+function createReverbWidget(){
+  const reverb_api = 'https://api.reverb.com/api/my/payments/selling'
 
+  fetch(reverb_api, {
+      method: 'GET',
+      headers: {
+          'Authorization': `Bearer ${api_storage.reverb.apiKey}`
+      }
+  }).then(function(response){
+      return response.json();
+  }).then(function(data){
+      var total_rev = 0;
+      var monthly_rev = 0;
+      for (var i in data.payments) {
+          var payment = data.payments[i].amount_item.amount_cents / 100
+          total_rev += payment
+          var date = data.payments[i].received_at
+          var month = dayjs(date).month()
+          if (month === currentMonth) {
+            monthly_rev += payment
+          }
+      }
+      //? Generate Reverb Widget 
+      const teachWidget = document.createElement("div");
+      teachWidget.setAttribute("class", "widget");
+      teachWidget.innerHTML = '<h1>Reverb</h1>' + 
+      '<div>Monthly Revenue: $'+ monthly_rev + '</div>' +
+      '<div>Total Revenue TD: $'+ total_rev + '</div>' + 
+      '<br/>'
+      test_widget.appendChild(teachWidget)
 
-fetch(reverb_api, {
-    method: 'GET',
-    headers: {
-        'Authorization': 'Bearer 6bface44d1572b16cdd61888bb393cff99142a84dc746d1a01934fd11aefea09'
-    }
-}).then(function(response){
-    return response.json();
-}).then(function(data){
-    var total_rev = 0;
-    var monthly_rev = 0;
-    for (var i in data.payments) {
-        var payment = data.payments[i].amount_item.amount_cents / 100
-        total_rev += payment
-        var date = data.payments[i].received_at
-        var month = dayjs(date).month()
-        if (month === currentMonth) {
-          monthly_rev += payment
-        }
-    }
-    const teachWidget = document.createElement("div");
-    teachWidget.setAttribute("class", "widget");
-    teachWidget.innerHTML = '<h1>Reverb</h1>' + 
-    '<div>Monthly Revenue: $'+ monthly_rev + '</div>' +
-    '<div>Total Revenue TD: $'+ total_rev + '</div>' + 
-    '<br/>'
-    test_widget.appendChild(teachWidget)
+  })
+}
 
-    
-})
 
 //*** Merch Widgets ****/
 //! T-Spring
