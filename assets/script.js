606e0f2990d361616d9404bbe6db0837ba88f1e6
1willcobb <cobb.will@gmail.com>
2023-06-17 10:39:52 -0500

    added chart and connected the backend to the chart data

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,7 @@ var goal_rev = 0;
 var api_storage;
 var reverb_monthly_rev = 0;
 var teachable_monthly_rev = 0;
-var influence_number = 1000;
+var influence_number = 0;
 
 //* Options Buttons
 const youtube_btn = document.getElementById('youtube');
@@ -87,6 +87,10 @@ var placeholderDataStructure = {
     widget_name: 'reverbWidget',
     apiKey: ''
   }
+}
+
+youtubePLaceholder = {
+  
 }
 
 //* App Startup
@@ -338,13 +342,13 @@ goals_btn.addEventListener("click", function(){
 //* Goal compared
 function getCompare() {
   console.log(api_storage.user.monthly_rev)
-  goal_rev =  (((reverb_monthly_rev + teachable_monthly_rev) / api_storage.user.monthly_rev)*100).toFixed(2)
-  influence_comp = (influence_number / api_storage.user.influence)*100
+  goal_rev =  parseInt((((reverb_monthly_rev + teachable_monthly_rev) / api_storage.user.monthly_rev)*100).toFixed(2))
+  influence_comp = parseInt(((influence_number / api_storage.user.influence)*100).toFixed(2))
   console.log("goal rev: " + goal_rev)
   console.log("influencer: " + influence_comp)
   percent_rev.textContent = goal_rev + '%'
   percent_inf.textContent = influence_comp + '%'
-  renderChart();
+  chartRender();
 }
 
 
@@ -371,6 +375,7 @@ function createYoutubeWidget() {
             return response.json();
           })
           .then(function (data){
+            console.log(data)
             var channelName = data.items[0].snippet.title
             var subscriberCount = data.items[0].statistics.subscriberCount
             var videoCount = data.items[0].statistics.videoCount
@@ -562,101 +567,99 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-//? CHART RENDER
-function renderChart() {
+chartRender();
 
-  Highcharts.chart('container', {
+function chartRender() {
+  //? CHART RENDER
+Highcharts.chart('container', {
 
-    chart: {
-        type: 'solidgauge',
-        height: '110%',
-    },
-  
-    title: {
-        text: 'Monthly Goals',
-        style: {
-            fontSize: '24px'
-        }
-    },
-  
-    tooltip: {
-        borderWidth: 0,
-        backgroundColor: 'none',
-        shadow: false,
-        style: {
-            fontSize: '16px'
-        },
-        valueSuffix: '%',
-        pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}</span>',
-        positioner: function (labelWidth) {
-            return {
-                x: (this.chart.chartWidth - labelWidth) / 2,
-                y: (this.chart.plotHeight / 2) + 15
-            };
-        }
-    },
-  
-    pane: {
-        startAngle: 0,
-        endAngle: 360,
-        background: [{ // Track for Move
-            outerRadius: '112%',
-            innerRadius: '89%',
-            backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0])
-                .setOpacity(0.3)
-                .get(),
-            borderWidth: 0
-        }, { // Track for Exercise
-            outerRadius: '87%',
-            innerRadius: '63%',
-            backgroundColor: Highcharts.color(Highcharts.getOptions().colors[1])
-                .setOpacity(0.3)
-                .get(),
-            borderWidth: 0
-        }]
-    },
-  
-    yAxis: {
-        min: 0,
-        max: 100,
-        lineWidth: 0,
-        tickPositions: []
-    },
-  
-    plotOptions: {
-        solidgauge: {
-            dataLabels: {
-                enabled: false
-            },
-            linecap: 'round',
-            stickyTracking: false,
-            rounded: true
-        }
-    },
-  
-    series: [{
-        name: 'Revenue',
-        data: [{
-            color: Highcharts.getOptions().colors[0],
-            radius: '112%',
-            innerRadius: '88%',
-            y: 55
-        }]
-    }, {
-        name: 'Influence',
-        data: [{
-            color: Highcharts.getOptions().colors[1],
-            radius: '87%',
-            innerRadius: '63%',
-            y: 36
-        }]
-    }]
-  });
-};
+  chart: {
+      type: 'solidgauge',
+      height: '110%',
+      backgroundColor: '#eff8ff',
+  },
 
+  title: {
+      text: 'Monthly Goals',
+      style: {
+          fontSize: '24px'
+      }
+  },
 
+  tooltip: {
+      borderWidth: 0,
+      backgroundColor: 'none',
+      shadow: false,
+      style: {
+          fontSize: '16px'
+      },
+      valueSuffix: '%',
+      pointFormat: '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}</span>',
+      positioner: function (labelWidth) {
+          return {
+              x: (this.chart.chartWidth - labelWidth) / 2,
+              y: (this.chart.plotHeight / 2) + 15
+          };
+      }
+  },
 
+  pane: {
+      startAngle: 0,
+      endAngle: 360,
+      background: [{ // Track for Move
+          outerRadius: '112%',
+          innerRadius: '89%',
+          backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0])
+              .setOpacity(0.3)
+              .get(),
+          borderWidth: 0
+      }, { // Track for Exercise
+          outerRadius: '87%',
+          innerRadius: '63%',
+          backgroundColor: Highcharts.color(Highcharts.getOptions().colors[1])
+              .setOpacity(0.3)
+              .get(),
+          borderWidth: 0
+      }]
+  },
 
+  yAxis: {
+      min: 0,
+      max: 100,
+      lineWidth: 0,
+      tickPositions: []
+  },
+
+  plotOptions: {
+      solidgauge: {
+          dataLabels: {
+              enabled: false
+          },
+          linecap: 'round',
+          stickyTracking: false,
+          rounded: true
+      }
+  },
+
+  series: [{
+      name: 'Revenue',
+      data: [{
+          color: Highcharts.getOptions().colors[0],
+          radius: '112%',
+          innerRadius: '88%',
+          y: goal_rev
+      }]
+  }, {
+      name: 'Influence',
+      data: [{
+          color: Highcharts.getOptions().colors[1],
+          radius: '87%',
+          innerRadius: '63%',
+          y: influence_comp
+      }]
+  }]
+});
+}
 
 
 
